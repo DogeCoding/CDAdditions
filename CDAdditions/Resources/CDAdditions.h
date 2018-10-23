9e5c4d581d89d0da6c6694e7f3fe11d0e7b6b31b
yangfukai_sx <yangfukai_sx@qiyi.com>
2018-10-23 17:22:12 +0800

    version 0.1.2

---
+++ CDAdditions/Resources/CDAdditions.h
@@ -0,0 +1,14 @@
+//
+//  CDAdditions.h
+//  CDAdditions
+//
+//  Created by 杨扶恺 on 2018/10/18.
+//  Copyright © 2018 杨扶恺. All rights reserved.
+//
+
+#import "CDConstant.h"
+#import "NSString+CDAdditions.h"
+#import "UIDevice+CDAdditions.h"
+#import "UIFont+CDAdditions.h"
+#import "UIView+CDAdditions.h"
+
